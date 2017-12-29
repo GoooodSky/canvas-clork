a55d26a20e8d38070536728a2f9cbe42effc9728
uchihaty <liu_tianyi@outlook.com>
2017-12-29 19:23:08 +0800

    ver 1.0.0
    official version 1.0.0

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-------
+#CVClork
+![clork](./img/clork.png)
 
-**A clork use Canvas and Javascript.**
+![browser](./img/browser.svg)
 
-------
+##A clork drawed by canvas
+CVClork a clork drawed by canvas.It's a quartz analog clork which can display time and date.
+
+##Change working mode
+Just click once on the dial plate,it will change time mode to date mode or vice versa.
+![clork](./img/clork.png)  <----Click---->  ![clork](./img/date.png)
+
+##License
+Copyright (c) 2017-present, Tianyi(Sky) Liu
--- js/date.js
+++ js/date.js
@@ -5,8 +5,6 @@ var height = ctx.canvas.height;
 var r = width / 2;
 var rem = width / 200;  //比例
 var d = new Date();
-
-
 function drawBackground(){
     ctx.save();
     ctx.translate( r, r);
@@ -14,41 +12,13 @@ function drawBackground(){
     ctx.lineWidth = 10 * rem;
     ctx.arc( 0, 0, r - ctx.lineWidth / 2, 0, 2 * Math.PI, false);    
     ctx.stroke();//空心
-
     ctx.font = 18 * rem + 'px Arial';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
-
     ctx.fillText(d.getFullYear(), -30, 0);
     ctx.fillText(".", -5, 0);
     ctx.fillText(d.getMonth()+1, 10, 0);
     ctx.fillText(".", 25, 0);
     ctx.fillText(d.getDate(), 40, 0);
-
-
-
-
 }
-
-
-
 drawBackground();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
