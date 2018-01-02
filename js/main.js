ae075a348ae5400eb788291dca60d5223f9ddd65
uchihaty <liu_tianyi@outlook.com>
2018-01-02 21:45:08 +0800

    Ver 1.0.1
    Vue.component

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
+Vue.component("canvas-clork",{
+	data:function(){
+		return{
+			isShow:true
+		}
+	},
+	template:`
+	<div v-on:click="isShow=!isShow">
+		<canvas id="clork" height="200px" width="200px" v-show="isShow"></canvas>
+		<canvas id="date" height="200px" width="200px" v-show="!isShow"></canvas>
+	</div>
+	`
+})
 new Vue({
-	el:"#main",
-	data:{
-		isShow:true
-	}
+	el:"canvas-clork",
 })
